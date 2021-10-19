7d64eb150a85416dbcfc7fffc64aaa436a8cb53b
Urfeen <urfeens@gmail.com>
2021-10-20 01:51:05 +0300

    fix: Htag typo fix

--- components/HTag/Htag.tsx
+++ components/HTag/Htag.tsx
@@ -7,9 +7,9 @@ export function Htag({ tag, children, className, ...props }: IHtagProps): JSX.El
     case 'h1':
       return <h1 className={cn(styles.h1, className)} {...props}>{children}</h1>;
     case 'h2':
-      return <h2 className={cn(styles.h1, className)} {...props}>{children}</h2>;
+      return <h2 className={cn(styles.h2, className)} {...props}>{children}</h2>;
     case 'h3':
-      return <h3 className={cn(styles.h1, className)} {...props}>{children}</h3>;
+      return <h3 className={cn(styles.h3, className)} {...props}>{children}</h3>;
     default:
       return <></>;
   }
