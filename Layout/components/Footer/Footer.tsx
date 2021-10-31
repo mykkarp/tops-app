131204053a0dbf1357ab94e0fe58154d1c4b7abf
mykkarp <mykkarp@gmail.com>
2021-11-01 00:04:21 +0300

    feat: footer

--- Layout/components/Footer/Footer.tsx
+++ Layout/components/Footer/Footer.tsx
@@ -1,11 +1,14 @@
 import IFooterProps from './IFooter.props';
 import styles from "./Footer.module.css";
 import cn from 'classnames';
+import { format } from 'date-fns';
 
-export function Footer({ ...props }: IFooterProps): JSX.Element {
+export function Footer({ className, ...props }: IFooterProps): JSX.Element {
   return (
-    <footer {...props}>
-      Footer
+    <footer className={cn(className, styles.footer)} {...props}>
+      <div><span className={styles.text}>OwlTop © 2020 - {format(new Date(), 'yyyy')} All rights reserved</span></div>
+      <div><a href="#" className={styles.text}>Terms of use</a></div>
+      <div><a href="#" className={styles.text}>Privacy Policy</a></div>
     </footer>
   );
 }
