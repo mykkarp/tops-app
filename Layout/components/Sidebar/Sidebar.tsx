ba8376db5f9abe036d2f772d79ddbaeeee4f7e49
mykkarp <mykkarp@gmail.com>
2021-11-13 15:06:23 +0300

    feat: aside styles

--- Layout/components/Sidebar/Sidebar.tsx
+++ Layout/components/Sidebar/Sidebar.tsx
@@ -2,10 +2,13 @@ import ISidebarProps from './ISidebar.props';
 import styles from "./Sidebar.module.css";
 import cn from 'classnames';
 import { Menu } from '../Menu/Menu';
+import Logo from '../../logo.svg';
 
-export function Sidebar({ ...props }: ISidebarProps): JSX.Element {
+export function Sidebar({ className, ...props }: ISidebarProps): JSX.Element {
   return (
-    <aside {...props}>
+    <aside className={cn(className, styles.sidebar)} {...props}>
+      <Logo className={styles.logo} />
+      <div>Поиск</div>
       <Menu />
     </aside>
   );
