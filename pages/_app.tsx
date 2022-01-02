08e5538e21280036413fd9d0d6080af519a3ffd8
mykkarp <mykkarp@gmail.com>
2022-01-02 14:58:54 +0300

    feat: meta tags

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,7 +2,7 @@ import '../styles/globals.css';
 import type { AppProps } from 'next/app';
 import Head from 'next/head';
 
-function MyApp({ Component, pageProps }: AppProps): JSX.Element {
+function MyApp({ Component, pageProps, router }: AppProps): JSX.Element {
   return (
     <>
       <Head>
@@ -11,6 +11,8 @@ function MyApp({ Component, pageProps }: AppProps): JSX.Element {
         <link rel="icon" href="/favicon.ico" />
         <link rel="preconnect" href="https://fonts.gstatic.com" />
         <link href="https://fonts.googleapis.com/css2?family=Noto+Sans+KR:wght@300;400;500;700&display=swap" rel="stylesheet" />
+        <meta property='og:url' content={process.env.NEXT_PUBLIC_DOMAIN + router.asPath} />
+        <meta property='og:locale' content='ru_RU' />
       </Head>
       <Component {...pageProps} />
     </>
